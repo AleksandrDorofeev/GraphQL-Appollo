16a5a382a0b7962aba07f00e674409d3d4e28d0c
Aleksandr <adorofeev@team.amocrm.com>
2018-10-29 02:58:51 +0300

    add yoga server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,30 @@
-console.log('Hello!')
+const { GraphQLServer } = require('graphql-yoga');
+const fetch = require('node-fetch');
+
+const baseURL = 'https://rickandmortyapi.com/api/';
+
+const typeDefs = `
+type Query {
+  getInfo(id: Int!): Info
+}
+type Info {
+  name: String,
+  air_date: String,
+  episode: String,
+  url: String,
+  created: String
+}
+`
+
+const resolvers = {
+  Query: {
+    // hello: (_, { name }) => `Hello ${name || 'World'}`,
+    getInfo: async (_, {id}) => {
+      const response = await fetch(baseURL + `/episode/${id}`)
+      return response.json();
+    }
+  },
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log('Server is running on localhost:4000'))
