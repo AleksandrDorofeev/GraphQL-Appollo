5f4b1a383569d89a777e2a3abf555347f001bec9
Aleksandr <adorofeev@team.amocrm.com>
2018-11-14 07:47:31 +0300

    change to swapi api

--- server/index.js
+++ server/index.js
@@ -1,26 +1,45 @@
 const { GraphQLServer } = require('graphql-yoga');
 const fetch = require('node-fetch');
 
-const baseURL = 'https://rickandmortyapi.com/api/';
+const baseURL = 'https://swapi.co/api';
 
 const typeDefs = `
 type Query {
-  getInfo(id: Int!): Info
+  getInfo(id: Int!): Person
 }
-type Info {
+
+type Film {
+  url: String
+}
+
+type Person {
   name: String,
-  air_date: String,
-  episode: String,
-  url: String,
-  created: String
+  height: Int,
+  mass: Int,
+  hair_color: String,
+  skin_color: String,
+  eye_color: String,
+	birth_year: String,
+	gender: String,
+  homeworld: String,
+  films: [Film]!
 }
 `
 
 const resolvers = {
+  Person: {
+    films: res => {
+      const promises = res.films.map(async url => {
+        const response = await fetch(url)
+        return response.json()
+      })
+      return Promise.all(promises)
+    }
+  },
   Query: {
     // hello: (_, { name }) => `Hello ${name || 'World'}`,
     getInfo: async (_, {id}) => {
-      const response = await fetch(baseURL + `/episode/${id}`)
+      const response = await fetch(baseURL + `/people/${id}`)
       return response.json();
     }
   },
